663ff984620edd0cf4b6d17f4c46da63f6d84fe1
aarifislam8494-star <aarifislam8494@gmail.com>
2026-05-01 18:08:33 +0600

    feat: deployment URI

--- src/app/profile/update/page.jsx
+++ src/app/profile/update/page.jsx
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { authClient } from "@/lib/auth-client";
+import { authClient } from "../../../lib/auth-client";
 import toast from "react-hot-toast";
 import Link from "next/link";
 
